441d6c07a67d76ec570a7eb386a0a4f78bbc6432
Agares <ekeisco@gmail.com>
2014-01-09 00:07:08 -0500

    agrega las plantillas para el contenido del sitio

--- app/server.js
+++ app/server.js
@@ -10,5 +10,13 @@ app.get('/', function(req, res){
   res.render('index', {title:'Explicando código - CodeCafe.tv'});
 });
 
+app.get('/video', function(req, res){
+  res.render('video', {title:'Explicando código - CodeCafe.tv'});
+});
+
+app.get('/sugerir', function(req, res){
+  res.render('suggest', {title:'Explicando código - CodeCafe.tv'});
+});
+
 app.listen(process.env.PORT || 3000);
 console.log('Listening on port %s', process.env.PORT || 3000);
